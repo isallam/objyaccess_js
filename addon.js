fdc878215ef18ae06961e3d2e70987c7573d3564
ibrahim sallam <ibe.sallam@gmail.com>
2017-10-26 17:52:29 -0400

    added another query

--- addon.js
+++ addon.js
@@ -2,9 +2,6 @@
 var addon = require('bindings')('addon');
 
 var objyAccess = new addon.ObjyAccess("/home/ibrahim/projects/blockparser/data/bitcoin.boot");
-//console.log( obj.plusOne() ); // 11
-//console.log( obj.plusOne() ); // 12
-//console.log( obj.plusOne() ); // 13
 
 console.log('querying FD: ', objyAccess.connection()); 
 
@@ -13,4 +10,10 @@ objyAccess.query("From Block return count(*)", function(msg) {
   console.log(msg);
 })
 
+var q2 = 'match p = (:Address {m_Hash == "1HB5XMLmzFVj8ALj6mfBsbifRoD4miY36v"})' +
+              '-->()-->(:Transaction)-->(:Input)-->(:Transaction) return p;';
+      
+objyAccess.query(q2, function(msg) {
+    console.log("Q2 Results: ", msg)
+})
 console.log('Done processing.');
